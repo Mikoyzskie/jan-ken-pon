2aeb664c9090b865328212c9bfecd220edf18e05
Mikoyzskie <rapidstrike13@gmail.com>
2025-04-11 16:16:02 +0800

    robin

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const scoreContainer = document.querySelector(".header__score");
-const stepsContainer = document.querySelector(".steps")
+const stepsContainer = document.querySelector(".steps");
+const fightContainer = document.querySelector(".fight");
 
 const scoreCount = scoreContainer.querySelector(".score__count");
 
@@ -17,7 +18,44 @@ if(stepsContainer){
   })
 }
 
+const steps = {
+ ROCK: "rock",
+ PAPER: "paper",
+ SCISSORS: "scissors",
+ LIZARD: "lizard",
+ SPOCK: "spock",
+};
 
+const stepToImage = {
+ [steps.ROCK]: "./assets/images/icon-rock.svg",
+ [steps.PAPER]: "./assets/images/icon-paper.svg",
+ [steps.SCISSORS]: "./assets/images/icon-scissors.svg",
+ [steps.LIZARD]: "./assets/images/icon-lizard.svg",
+ [steps.SPOCK]: "./assets/images/icon-spock.svg",
+};
+
+
+function PickStep(step){
+
+  if(stepsContainer && fightContainer){
+    stepsContainer.classList.add("hidden");
+    fightContainer.classList.remove("hidden");
+
+    const userPicked = fightContainer.querySelector(".step__picked");
+    const pickedIcon = document.createElement("img");
+    pickedIcon.className = "circle__icon";
+    pickedIcon.src = stepToImage[step]
+    userPicked.appendChild(pickedIcon);
+    userPicked.classList.add(step)
+  }
+
+  // switch(step){
+  //   case "scissor":
+
+  //     break;
+  // }
+  
+}
 
 function PlaySound(soundobj) {
  var thissound = document.getElementById(soundobj);
@@ -28,4 +66,8 @@ function StopSound(soundobj) {
  var thissound = document.getElementById(soundobj);
  thissound.pause();
  thissound.currentTime = 0;
+}
+
+function RandomNumber(){
+  return Math.floor(Math.random() * 5);
 }
